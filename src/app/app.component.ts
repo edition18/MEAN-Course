7db6c3bb729fa3552233c21879f711e9dd420b11
Edwin Choo <jchoo004@e.ntu.edu.sg>
2020-09-27 18:15:32 +0800

    lesson 24 midway

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MEAN-Course';
+  posts = [];
+
+
+  onPostAdded(post) {
+    this.posts.push(post);
+  }
 }
