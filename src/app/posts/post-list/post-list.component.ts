5c866cae867da0e5f63e111e55e72be972dc0ad2
Edwin Choo <jchoo004@e.ntu.edu.sg>
2020-09-27 14:45:40 +0800

    first

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+
+export class PostListComponent {
+  posts = [
+    // { title: "1st post", content: "xxx "},
+    // { title: "1nd post", content: "yyy "},
+    // { title: "3rd post", content: "zzz "}
+
+  ];
+}
