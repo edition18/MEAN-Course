daf2ee00688b9dbe32565357a90edf8ea7a2f38b
Edwin Choo <jchoo004@e.ntu.edu.sg>
2020-10-24 08:42:43 +0800

    lesson 53 connected MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
 const Post = require('../models/post');
 
 
 const app = express();
 // big chain of middleware/funnel
+
+
+//pw kT28HDmiZKcmPrwp
+mongoose.connect("mongodb://edwin:kT28HDmiZKcmPrwp@cluster0-shard-00-00.0bvjz.mongodb.net:27017,cluster0-shard-00-01.0bvjz.mongodb.net:27017,cluster0-shard-00-02.0bvjz.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-il0d9v-shard-0&authSource=admin&retryWrites=true&w=majority")
+  .then(() => {
+    console.log("Connected to DB!");
+  }).catch(() => {
+    console.log("Connection failed");
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
