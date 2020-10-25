10c155bbeb866aea68a82e859df178ca903216db
Edwin Choo <jchoo004@e.ntu.edu.sg>
2020-10-25 10:47:45 +0800

    finished lesson 68

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -39,7 +39,8 @@ export class PostsService {
   } // so that we can listen to the private postUpdated
 
   getPost(id: string) {
-    return {...this.posts.find(p => p.id === id)}
+    return this.http.get<{_id: string, title: string, content: string }>("http://localhost:3000/api/posts/" + id);
+    // this is now a observable
   }
 
   addPost(title: string, content: string) {
@@ -58,12 +59,14 @@ export class PostsService {
   updatePost(id:string, title: string, content: string){
     const post: Post = {id: id, title: title, content: content}
     this.http.put("http://localhost:3000/api/posts/" + id, post)
-      .subscribe(response => console.log(response));
+      .subscribe(response => {
+        const updatedPosts = [...this.posts];
+        const oldPostIndex = updatedPosts.findIndex(p => p.id == post.id);
+        updatedPosts[oldPostIndex] = post;
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
+      });
   }
-
-
-
-
 
   deletePost(postId: string) {
     this.http.delete("http://localhost:3000/api/posts/" + postId)
@@ -74,7 +77,5 @@ export class PostsService {
       });
   }
 
-
-
 }
-// HbMZtkkJ0ajOKaxX
+
