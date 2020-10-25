1311e10f82af8e61177fc90ca469306fc5dcf239
Edwin Choo <jchoo004@e.ntu.edu.sg>
2020-10-25 11:27:24 +0800

    lesson 69 cleaned up backend

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const Post = require('../models/post');
+
+const postsRoutes = require("./routes/posts")
 
 const app = express();
 // big chain of middleware/funnel
@@ -25,59 +26,7 @@ app.use((req, res, next) => { //add headers to bypass CORS error
   next(); //so it moves on to next middleware
 });
 
-app.post("/api/posts", (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  }); // .body is added due to body parser
-  post.save().then(createdPost => {
-      res.status(201).json({
-      message: "Post added successfully",
-      postId: createdPost._id
-    }); // 200 means everything ok
-    //201 means we added new resource
-    //no next() as we already have a response
-
-  });
-});
-
-app.put("/api/posts/:id", (req, res, next) => {
- const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content
- });
-  Post.updateOne({_id: req.params.id}, post).then(result => {
-    console.log(result);
-    res.status(200).json({ message: "Update Succesful"});
-  })
-});
-
-app.get("/api/posts/:id", (req, res, next) => {
-  Post.findById(req.params.id).then(post => {
-    if (post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({message: "Post not found!"});
-    }
-  })
-});
-
-app.get("/api/posts", (req,res,next) => {
-  Post.find().then(documents => {
-    res.status(200).json({
-      message: "Posts fetched successfully",
-      posts: documents
-    }); // find allows u to find all or specific item in collection
-  });
-});
-
-app.delete("/api/posts/:id", (req,res,next) => {
- Post.deleteOne({_id: req.params.id}).then(result => {
-  console.log(result);
-  res.status(200).json({messsage: "Post deleted"});
- });
-});
+app.use("/api/posts", postsRoutes);
 
 
 module.exports = app;
