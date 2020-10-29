53343cb623da99a078970be77415f1969f3c7ffe
Edwin Choo <jchoo004@e.ntu.edu.sg>
2020-10-29 22:15:32 +0800

    lesson 84 allow for image change for post edits

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -48,12 +48,20 @@ router.post("", multer({storage: storage}).single("image") ,(req, res, next) => 
   });
 });
 
-router.put("/:id", (req, res, next) => {
+router.put("/:id", multer({storage: storage}).single("image"), (req, res, next) => {
+  let imagePath = req.body.imagePath;
+  if (req.file) {
+    // if this is truthy, a new file was uploaded
+    const url = req.protocol + "://" + req.get("host");
+    imagePath = url + "/images/" + req.file.filename;
+  }
   const post = new Post({
      _id: req.body.id,
      title: req.body.title,
-     content: req.body.content
+     content: req.body.content,
+     imagePath: imagePath
   });
+  console.log(post);
    Post.updateOne({_id: req.params.id}, post).then(result => {
      console.log(result);
      res.status(200).json({ message: "Update Succesful"});
