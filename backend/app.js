87c32a7d4dabc08db164d2cf4fb2b2c11da8f01c
Edwin Choo <jchoo004@e.ntu.edu.sg>
2020-10-31 13:49:36 +0800

    lesson 99

--- backend/app.js
+++ backend/app.js
@@ -10,13 +10,15 @@ const app = express();
 // big chain of middleware/funnel
 
 //pw kT28HDmiZKcmPrwp
-mongoose.connect("mongodb://edwin:edwin@cluster0-shard-00-00.0bvjz.mongodb.net:27017,cluster0-shard-00-01.0bvjz.mongodb.net:27017,cluster0-shard-00-02.0bvjz.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-il0d9v-shard-0&authSource=admin&retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://edwin:edwin@cluster0.0bvjz.mongodb.net/node-angular")
   .then(() => {
     console.log("Connected to DB!");
   }).catch(() => {
     console.log("Connection failed");
   });
-
+//mongodb+srv://edwin:<password>@cluster0.0bvjz.mongodb.net/<dbname>?retryWrites=true&w=majority
+//mongodb://edwin:edwin@cluster0-shard-00-00.0bvjz.mongodb.net:27017,cluster0-shard-00-01.0bvjz.mongodb.net:27017,cluster0-shard-00-02.0bvjz.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-il0d9v-shard-0&authSource=admin&retryWrites=true&w=majority
+//remove retry rewrite from the mongoURL
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false })); //for URL encoded data
 app.use("/images", express.static(path.join("backend/images")));
