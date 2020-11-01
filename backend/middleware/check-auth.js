99b8f81d9741d1c1475e40f2a3cbcf7cdd229540
Edwin Choo <jchoo004@e.ntu.edu.sg>
2020-11-01 14:33:10 +0800

    lesson 116 complete

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -5,7 +5,8 @@ module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
     // "Bearer fsdsaidsaldadansoipdna"
-    jwt.verify(token, "secret_this_should_be_longer");
+    const decodedToken = jwt.verify(token, "secret_this_should_be_longer");
+    req.userData = {email: decodedToken.email, userId: decodedToken.userId};
     next(); // travel on request
   } catch(error) {
     res.status(401).json({ message: "auth failed! "});
