68313f0bc4c6bb0a7632361e715641c8a0279983
Edwin Choo <jchoo004@e.ntu.edu.sg>
2020-11-01 10:24:11 +0800

    lesson 107 UI component changed when logged in

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,16 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from "@angular/core";
+import { Subject } from 'rxjs';
 import { AuthData } from './auth-data.model';
 
 @Injectable({ providedIn: "root" })
 
 export class AuthService {
   private token: string;
+  authStatusListener = new Subject<boolean>();
 
   constructor(private http: HttpClient) {}
 
   getToken(){
     return this.token;
+  }
+
+  getAuthStatusListener() {
+    return this.authStatusListener.asObservable();
   }
 
   createUser(email: string, password: string) {
@@ -26,6 +32,7 @@ export class AuthService {
     this.http.post<{token: string}>("http://localhost:3000/api/user/login", authData).subscribe(response => {
       const token = response.token;
       this.token = token;
+      this.authStatusListener.next(true);
     });
   }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,27 @@
-import { Component } from "@angular/core"
+import { Component, OnInit } from "@angular/core"
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit{
+  private authListenerSubs: Subscription;
+  userIsAuthenticated = false;
+  constructor(private authService: AuthService) {}
+
+
+
+  ngOnInit() {
+    this.authListenerSubs = this.authService.getAuthStatusListener().subscribe(
+      isAuthenticated => {
+        this.userIsAuthenticated = isAuthenticated
+      });
+
+  }
+
+
+  ngOnDestroy() {
+  }
+}
