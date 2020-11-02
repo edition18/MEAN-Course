bb542aef41f041fb653020cacffd1c89fdcfda03
Edwin Choo <jchoo004@e.ntu.edu.sg>
2020-11-02 21:51:52 +0800

    lesson 138

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -4,6 +4,8 @@ import { Post } from './post.model';
 import { Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { environment } from "../../environments/environment";
+const BACKEND_URL = environment.apiUrl + "/posts/";
 
 
 @Injectable({providedIn: 'root'})
@@ -17,7 +19,7 @@ export class PostsService {
     const queryParams = `?pagesize=${postsPerPage}&page=${currentPage}`;
     // you have named the HttpClient object as http as a privatge property here
     this.http.get<{message: string, posts: any, maxPosts: number }>(
-        "http://localhost:3000/api/posts" + queryParams
+        BACKEND_URL + queryParams
       )
       .pipe(
         map(postData => {
@@ -47,7 +49,7 @@ export class PostsService {
   } // so that we can listen to the private postUpdated
 
   getPost(id: string) {
-    return this.http.get<{_id: string, title: string, content: string, imagePath: string, creator: string}>("http://localhost:3000/api/posts/" + id);
+    return this.http.get<{_id: string, title: string, content: string, imagePath: string, creator: string}>(BACKEND_URL + id);
     // this is now a observable
   }
 
@@ -56,7 +58,7 @@ export class PostsService {
     postData.append("title", title);
     postData.append("content", content);
     postData.append("image", image, title);
-    this.http.post<{ message: string, post: Post }>("http://localhost:3000/api/posts", postData).subscribe(responseData => {
+    this.http.post<{ message: string, post: Post }>(BACKEND_URL, postData).subscribe(responseData => {
       this.router.navigate(["/"]);
     });
   }
@@ -80,14 +82,14 @@ export class PostsService {
         //prevent user from manipulating
       };
     }
-    this.http.put("http://localhost:3000/api/posts/" + id, postData)
+    this.http.put(BACKEND_URL + id, postData)
       .subscribe(response => {
         this.router.navigate(["/"]);
       });
   }
 
   deletePost(postId: string) {
-    return this.http.delete("http://localhost:3000/api/posts/" + postId);
+    return this.http.delete(BACKEND_URL + postId);
   }
 
 }
